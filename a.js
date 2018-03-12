500e6c17edc9f8d8b358e664c8a2cb373bea90ff
Kevin Hua <boogerlad@gmail.com>
2018-03-11 22:14:13 -0400

    too many fucking web standards

--- a.js
+++ a.js
@@ -4,7 +4,9 @@
 let canvas = document.getElementById('smoothie')
 
 
+document.getElementById("smoothie").style.width = window.innerWidth - 50 + 'px';
 
+document.getElementById("smoothie").style.height = window.innerHeight - 100 + 'px';
 
 let venus;
 let beginning = false;
@@ -19,7 +21,7 @@ document.getElementById('connect').onclick = async function() {
 	await venus.gatt.connect();
 	let service = await venus.gatt.getPrimaryService("6e400001-b5a3-f393-e0a9-e50e24dcca9e");
 	let characteristic = await service.getCharacteristic("6e400003-b5a3-f393-e0a9-e50e24dcca9e");
-	smoothie = new SmoothieChart({minValue: 30000, maxValue: 50000, millisPerPixel:10, responsive: true, labels:{/*fillStyle:'#000000'*/disabled:true}, grid:{fillStyle:'#FFFFFF'}});
+	smoothie = new SmoothieChart({minValue: 30000, maxValue: 50000, millisPerPixel:10, labels:{/*fillStyle:'#000000'*/disabled:true}, grid:{fillStyle:'#FFFFFF'}});
 	smoothie.streamTo(canvas);
 	smoothie.addTimeSeries(ir, { strokeStyle:'rgb(255, 0, 0)', fillStyle:'rgba(255, 0, 0, 0.2)', lineWidth:3 })
 	smoothie.addTimeSeries(temperature, { strokeStyle:'rgb(0, 0, 255)', fillStyle:'rgba(0, 0, 255, 0.2)', lineWidth:3 });
