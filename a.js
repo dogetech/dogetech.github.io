9f227a9d2218b3324c76a43ad8a558b82ef45b3a
Kevin Hua <boogerlad@gmail.com>
2018-03-11 22:20:06 -0400

    what is default

--- a.js
+++ a.js
@@ -4,10 +4,6 @@
 let canvas = document.getElementById('smoothie')
 
 
-document.getElementById("smoothie").style.width = window.innerWidth - 50 + 'px';
-
-document.getElementById("smoothie").style.height = window.innerHeight - 100 + 'px';
-
 let venus;
 let beginning = false;
 let stuff = [];
